a044ea55eeadf491f6473253d9399341bf617bf2
Shamil Khaibullov <shamil@shamil.dev>
2025-09-23 16:52:23 +0100

    daisyUI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export function App() {
-  return <h1 className="text-blue-600">Vite + React + TypeScript</h1>;
+  return (
+    <div>
+      <h1 className="text-blue-600">Vite + React + TypeScript</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
 }
