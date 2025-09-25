21e89a255ce10e29da7e626eebcffb8b0555030d
Shamil Khaibullov <shamil@shamil.dev>
2025-09-25 16:54:52 +0100

    ui: add hero

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 export function HomePage() {
   return (
-    <div className="h-screen">
-      <h1>Welcome to the Homepage!</h1>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/djqdtvv7u/image/upload/v1758814784/woman-in-top-and-shorts_si05mb.png)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl lg:text-6xl font-bold">
+            Effortless Style, Defined.
+          </h1>
+          <p className="mb-5 text-base lg:text-lg">
+            Check out the latest arrivals
+          </p>
+          <Link to="/products" className="btn btn-outline text-base lg:text-lg">
+            Shop Now
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
