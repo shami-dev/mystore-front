7d0e24934ab7fca68822cfe12d197cac0429502e
Shamil Khaibullov <shamil@shamil.dev>
2025-10-13 15:26:43 +0100

    fix height for the loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
 export function Loader() {
   return (
-    <div className="min-h-full flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg text-gray-600"></span>
     </div>
   );
